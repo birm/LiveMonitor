734e7e01040826facd1ade16cfebe7f6b22b6336
Ryan Birmingham <birm@gatech.edu>
2016-05-30 04:59:20 -0400

    new version information

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from distutils.core import setup
 files=["Monitors/*"]
 
 setup(name='LiveMonitor',
-    version="1.1",
-    description="Live async checks on the status of your network services and devices. Check port status, see networked host information, and check log summaries. API and application. Working on testing, API engine, and sample user interface. Will put into beta when testing complete",
+    version="1.2.2",
+    description="Live async checks on the status of your network services and devices. Check port status, see networked host information, and check log summaries. API and application. Working on testing, API engine, and sample user interface.",
     author="Ryan Birmingham",
+    author_email="birm@rbirm.us",
     url="http://rbirm.us",
     packages=['LiveMonitor'],
     package_data={'LiveMonitor':files},
