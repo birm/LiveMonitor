cb1f2e89e5ed937805aa437ec65806885c761cc3
birm <birm@gatech.edu>
2016-06-13 23:42:01 -0400

    fix dependencies

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 files=["Monitors/*"]
 
 setup(name='LiveMonitor',
-    version="1.2.2",
+    version="1.2.3",
     description="Live async checks on the status of your network services and devices. Check port status, see networked host information, and check log summaries. API and application. Working on testing, API engine, and sample user interface.",
     author="Ryan Birmingham",
     author_email="birm@rbirm.us",
@@ -11,7 +11,7 @@ setup(name='LiveMonitor',
     packages=['LiveMonitor'],
     package_data={'LiveMonitor':files},
     long_description=open('README').read(),
-    install_requires=["netaddr","logging", "time", "threading", "sockets", "json", "subprocess"],
+    install_requires=["netaddr"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Information Technology",
